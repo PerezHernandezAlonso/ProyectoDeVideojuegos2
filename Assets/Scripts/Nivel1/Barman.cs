1b46d536ad4a7520c9118ed8f0d44f136a8ce6f4
Alonso Perez Hernandez <perezhernandezalonso.dev@gmail.com>
2024-02-15 21:34:31 +0100

    Barman menu added

--- Assets/Scripts/Nivel1/Barman.cs
+++ Assets/Scripts/Nivel1/Barman.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Barman : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private bool isColliding = false;
+    public GameObject menu;
+    private bool MenuIsActive = false;
+
+    private void Update()
     {
+        if (isColliding == true && Input.GetKeyDown(KeyCode.F))
+        {
+            if (MenuIsActive == false)
+            {
+                LoadMenu();
+            } else
+            {
+                UnloadMenu();
+            }
+        }
+        if (isColliding == false)
+        {
+            UnloadMenu();
+        }
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+            isColliding = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            isColliding = false;
+    }
+
+    private void LoadMenu()
+    {
+        menu.SetActive(true);
+        MenuIsActive = true;
+    }
+    private void UnloadMenu()
+    {
+        menu.SetActive(false);
+        MenuIsActive = false;
     }
 }
