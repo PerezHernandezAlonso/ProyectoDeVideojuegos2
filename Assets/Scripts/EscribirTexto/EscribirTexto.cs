8978b32c085750a0f43b840aa9fa9501bff37a13
DavidYdev <davidyuhaowengwang@gmail.com>
2024-03-14 16:51:23 +0100

    EscenaComicServilleta

--- Assets/Scripts/EscribirTexto/EscribirTexto.cs
+++ Assets/Scripts/EscribirTexto/EscribirTexto.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,21 +5,27 @@ using UnityEngine.UI;
 
 public class EscribirTextoNarrador : MonoBehaviour
 {
-    string frase = "Vivía en una humilde casa, sin muchas florituras. Aquel perro era tan inteligente como persistente. Se dedicaba a investigar y resolver crímenes en las ciudades italianas. Durante años fue el pionero entre los detectives, hasta que un día tuvo un fortuito accidente que lo dejó invidente de por vida.. Una historia realmente trágica. Su nombre, Sloof Dogmes. Habían pasado 30 años desde aquel suceso y no descansó ni un sólo día. Andaba tras la pista del delincuente más famoso de aquellos tiempos, un contrabandista de alcohol que se jactó de estar en contra de la ley seca, el escurridizo Al Catpone. Ambos dejaron una gran huella dolorosa e imborrable en el otro. No existía un sentimiento de venganza entre ellos, sino ganas de demostrar de qué son realmente capaces.";
+    public List<string> frasesNarrador = new List<string>();
     public Text texto;
+    public int indiceTextoSeleccionado = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        frasesNarrador.Add("Vivía en una humilde casa, sin muchas florituras. Aquel perro era tan inteligente como persistente. Se dedicaba a investigar y resolver crímenes en las ciudades italianas. Durante años fue el pionero entre los detectives, hasta que un día tuvo un fortuito accidente que lo dejó invidente de por vida.. Una historia realmente trágica. Su nombre, Sloof Dogmes. Habían pasado 30 años desde aquel suceso y no descansó ni un sólo día. Andaba tras la pista del delincuente más famoso de aquellos tiempos, un contrabandista de alcohol que se jactó de estar en contra de la ley seca, el escurridizo Al Catpone. Ambos dejaron una gran huella dolorosa e imborrable en el otro. No existía un sentimiento de venganza entre ellos, sino ganas de demostrar de qué son realmente capaces.");
+        frasesNarrador.Add("Busca la llave que abre el cofre de los recuerdos perdidos, donde la risa es moneda de cambio y las melodías guardan secretos. En el lugar donde el humo se mezcla con las letras y las luces bailan al ritmo de la verdad oculta");
+
         StartCoroutine(Reloj());
     }
 
     IEnumerator Reloj()
     {
-        foreach (char caracter in frase)
+        string fraseActual = frasesNarrador[indiceTextoSeleccionado];
+
+        foreach (char caracter in fraseActual)
         {
-            texto.text = texto.text + caracter;
-            yield return new WaitForSeconds(0.08f);
+            texto.text += caracter;
+            yield return new WaitForSeconds(0.04f);
         }
     }
 }
--- Assets/Scripts/NivelComic/CamComic.cs
+++ Assets/Scripts/NivelComic/CamComic.cs
@@ -1,57 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CamComic : MonoBehaviour
 {
-    // Arreglo de Transform que contiene los puntos de vista de la cámara
     public Transform[] views;
-
-    // Velocidad de transición entre los puntos de vista
     public float transitionSpeed;
 
-    // Punto de vista actual
     private Transform currentView;
-
-    // Índice del punto de vista actual en el arreglo views
     private int currentIndex = 0;
-
-    // Variable para controlar si las vistas ya han cambiado una vez
     private bool viewsChanged = false;
+    public Canvas textoCanvas;
 
     void Start()
     {
-        // Establece el punto de vista actual al primer punto de vista en el arreglo views
         currentView = views[0];
-
-        // Coloca la cámara en la posición del punto de vista actual
         transform.position = currentView.position;
-
-        // Inicia la corutina para cambiar automáticamente los puntos de vista
         StartCoroutine(ChangeViewAutomatically());
     }
 
-    // Corutina para cambiar automáticamente los puntos de vista
     IEnumerator ChangeViewAutomatically()
     {
-        // Mientras las vistas no hayan cambiado y no se haya alcanzado el último punto de vista
         while (!viewsChanged && currentIndex < views.Length - 1)
         {
-            // Espera 3 segundos antes de cambiar al siguiente punto de vista
-            yield return new WaitForSeconds(3f);
-
-            // Incrementa el índice para cambiar al siguiente punto de vista
+            yield return new WaitForSeconds(4f);
             currentIndex++;
-
-            // Actualiza el punto de vista actual
             currentView = views[currentIndex];
+        }
+
+        // Si la cámara ha alcanzado el último punto de vista, muestra el Canvas con el texto después de esperar 4 segundos
+        if (currentIndex == views.Length - 1)
+        {
+            yield return new WaitForSeconds(4f);
+            ShowTextCanvas();
         }
     }
 
-    // Método LateUpdate se ejecuta después de que todos los objetos hayan sido procesados en la actualización de fotograma
     private void LateUpdate()
     {
-        // Actualiza gradualmente la posición de la cámara hacia la posición del punto de vista actual
         transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+    }
+
+    // Método para mostrar el Canvas con el texto
+    void ShowTextCanvas()
+    {
+        textoCanvas.gameObject.SetActive(true);
     }
 }
