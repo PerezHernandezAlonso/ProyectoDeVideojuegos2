d38d8c4c767f063072d5a8384736149cb8ecb478
Mayte <mayte.are.mar@gmail.com>
2024-04-24 18:31:38 +0200

    Revert "TextoCambiado"
    This reverts commit ad7d97db3aa4910a4ad8c5887d5ecedf3a71110f.

--- Assets/Scripts/EscribirTexto/EscribirTexto.cs
+++ Assets/Scripts/EscribirTexto/EscribirTexto.cs
@@ -12,8 +12,8 @@ public class EscribirTextoNarrador : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        frasesNarrador.Add("Vivía en una humilde casa, sin muchas florituras. Aquel perro era tan inteligente como persistente. Se dedicaba a investigar y resolver crímenes en las ciudades italianas. Durante años fue el pionero entre los detectives, hasta que un día tuvo un fortuito accidente que lo dejó invidente de por vida.. Una historia realmente trágica. Su nombre, Sloof Dogmes.");
-        frasesNarrador.Add("¡SORPRESA! ¿Creías que me había olvidado de ti? La vida sería muy aburrida sin mi querido amigo Sloof persiguiéndome allá donde vaya... ¿Quieres encontrar a la gallina de los huevos de oro? Pues estás de suerte. Estoy tan cansado de que todo sea tan fácil que he dejado algo interesante para ti en ese bar al que solíamos ir de jóvenes. ¿Lo recuerdas?");
+        frasesNarrador.Add("Vivía en una humilde casa, sin muchas florituras. Aquel perro era tan inteligente como persistente. Se dedicaba a investigar y resolver crímenes en las ciudades italianas. Durante años fue el pionero entre los detectives, hasta que un día tuvo un fortuito accidente que lo dejó invidente de por vida.. Una historia realmente trágica. Su nombre, Sloof Dogmes. Habían pasado 30 años desde aquel suceso y no descansó ni un sólo día. Andaba tras la pista del delincuente más famoso de aquellos tiempos, un contrabandista de alcohol que se jactó de estar en contra de la ley seca, el escurridizo Al Catpone. Ambos dejaron una gran huella dolorosa e imborrable en el otro. No existía un sentimiento de venganza entre ellos, sino ganas de demostrar de qué son realmente capaces.");
+        frasesNarrador.Add("Busca la llave que abre el cofre de los recuerdos perdidos, donde la risa es moneda de cambio y las melodías guardan secretos. En el lugar donde el humo se mezcla con las letras y las luces bailan al ritmo de la verdad oculta");
 
         StartCoroutine(Reloj());
     }
